2cc640d7c9ade1440caeaa3aad14ca65e740c7d6
Pablo Del Pino <pablodpmenmanuel@gmail.com>
2022-08-08 13:49:50 -0500

    01-Seccion-Terminada

---
+++ 01-Seccion/main.py
@@ -0,0 +1,8 @@
+print("¡Bienvenido al Generador de Nombre de Bandas!")
+cuidad = input("¿cual es el nombre de la ciudad que naciste?\n")
+mascota = input("¿Cual es el nombre de tu mascota?\n")
+print("El nombre de tu banda sera:" + cuidad + " " + mascota)
+
+c = cuidad
+cuidad = mascota
+mascota= c
